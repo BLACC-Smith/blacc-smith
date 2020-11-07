63fbb4053d822d4db5e8f9d0b395f8f7d5017dcf
Garrett Tolbert <garrett1.tolbert@gmail.com>
2020-11-07 03:23:22 -0600

    added throw to utility function

--- functions/utilities.js
+++ functions/utilities.js
@@ -1,4 +1,9 @@
+const { database } = require('firebase-admin');
+
 exports.randomIndex = (maxIndex) => Math.round(Math.random() * maxIndex);
 exports.removeFromList = (list, itemsToRemove) =>
 	list.filter((item) => !itemsToRemove.includes(item));
-exports.getRandomElement = (list) => list[this.randomIndex(list.length)];
+exports.getRandomElement = (list) => {
+	if (!list.length) throw { getRandomElement: 'List is empty' };
+	return list[this.randomIndex(list.length)];
+};
