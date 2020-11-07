36ffa375c7503ec55f5eefcdad192a46e1409371
Mikayla <mikayla.orange@bison.howard.edu>
2020-11-07 16:17:20 -0500

    WIP: Add POST request to create issues in GitHub

--- functions/githubRequests/githubRequests.js
+++ functions/githubRequests/githubRequests.js
@@ -5,5 +5,11 @@
 // format came from but in production, we will.
 exports.handleNewRequest = (request) => {
 	// Send request to github api
+	//const req = await postRequest(request)
 	return `New Issue: ${request.charAt(0).toUpperCase()}${request.slice(1)}`;
+};
+
+const postRequest = (request) => {
+	const url = 'https://api.github.com/repos/BLACC-Smith/blacc-smith/issues'
+	$.post(url, request)
 };
