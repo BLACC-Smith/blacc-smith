8a8b8def683fed014b714db37b26ab7454fc96c3
Mikayla <mikayla.orange@bison.howard.edu>
2020-11-07 20:43:20 -0500

    Update

--- functions/githubRequests/githubRequests.js
+++ functions/githubRequests/githubRequests.js
@@ -3,12 +3,12 @@ const { blaccSmithChannel } = require("../constants");
 
 exports.handleNewIssue = async ({ channel, issue, access_token }) => {
   try {
-    if (channel.id === blaccSmithChannel) {
-      return await postIssue({ issue, access_token });
+    if (channel.id !== blaccSmithChannel) {
+      return channel.send(
+        "All feature requests must be sent to the `#blacc-smith` channel"
+      );
     }
-    channel.send(
-      "All feature requests must be sent to the `#blacc-smith` channel"
-    );
+    return await postIssue({ issue, access_token });
   } catch (error) {
     throw { handleNewIssue: error };
   }
