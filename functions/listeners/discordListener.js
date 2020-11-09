64310ff2ac0bb63dfbf574c44b751f6bc19feaec
Garrett Tolbert <garrett1.tolbert@gmail.com>
2020-11-08 19:25:45 -0600

    added config file to listeners/

--- functions/listeners/discordListener.js
+++ functions/listeners/discordListener.js
@@ -1,20 +1,12 @@
-const Discord = require('discord.js');
-const functions = require('firebase-functions');
 const { handleAFAF } = require('./afaf');
+const { discordClient } = require('./config');
 
-// DISCORD CLIENT
-const discordClient = new Discord.Client();
-const { access_token } = functions.config().discord;
-discordClient.login(access_token).catch((err) => {
-	console.log(err)
-});
-
-module.exports = ()=>{
-  discordClient.on('message', async (message) => {
-  	try {
-  		await handleAFAF(message);
-  	} catch (error) {
-  		throw 'Error occured while asking for a friend';
-  	}
-  })
-}
+module.exports = () => {
+	discordClient.on('message', async (message) => {
+		try {
+			await handleAFAF(message);
+		} catch (error) {
+			throw 'Error occured while asking for a friend';
+		}
+	});
+};
