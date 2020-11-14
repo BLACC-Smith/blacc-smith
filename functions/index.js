138f292d390a86a2c6a90b789dd468361ed56a38
Garrett Tolbert <garrett1.tolbert@gmail.com>
2020-11-14 16:54:02 -0600

    removed unnecessary imports

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,7 @@
 var schedule = require('node-schedule');
-const functions = require('firebase-functions');
-const { scheduledJob } = require('./utilities');
 const discordListener = require('./listeners');
 const { handleDailyCC } = require('./automations/dailyCC');
 const { handleAffirmation } = require('./automations/affirmations');
-const { discordClient } = require('./config');
 
 discordListener();
 
