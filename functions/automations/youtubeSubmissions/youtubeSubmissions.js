eb87c726e9c2f09a61de329992493caeee096cdf
Garrett Tolbert <garrett1.tolbert@gmail.com>
2021-01-08 01:48:47 -0600

    added messageEmbed

--- functions/automations/youtubeSubmissions/youtubeSubmissions.js
+++ functions/automations/youtubeSubmissions/youtubeSubmissions.js
@@ -1,8 +1,18 @@
+const { MessageEmbed } = require('discord.js');
+
 exports.handleYoutubeSubmission = async (channel) => {
 	try {
-		await channel.send('📺 New Submission Alert');
+		await channel.send(embedMessage());
 		console.log('Alerted moderators');
 	} catch (error) {
 		throw { handleYoutubeSubmissions: error };
 	}
+};
+
+const embedMessage = () => {
+	return new MessageEmbed()
+		.setColor('#c4302b')
+		.setTitle('📺 New Submission Alert')
+		.setURL('https://admin.blacc.xyz/')
+		.setTimestamp();
 };
